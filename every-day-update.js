aef3a21080327c4751308439a747160c0ab20379
cupoftea4 <90093980+cupoftea4@users.noreply.github.com>
2023-02-16 20:44:13 +0200

    .

--- every-day-update.js
+++ every-day-update.js
@@ -1,6 +1,3 @@
-import axios from 'axios';
+import { doStudentScheduleParsing } from './data';
 
-// axios.get("https://raw.githubusercontent.com/cupoftea4/timetable-data/data/timetables/ПВ-33.json", {
-//   responseType: 'json',
-// }).then(response => response.data).then(console.log);
-// doStudentScheduleParsing();
+doStudentScheduleParsing();
