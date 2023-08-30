bdfeb0002919d994493dc1d8ba5c99685065911a
cupoftea4 <nadiia.telko.pz.2021@lpnu.ua>
2023-08-31 00:55:49 +0300

    most most most

--- src/utils/update-data.ts
+++ src/utils/update-data.ts
@@ -167,7 +167,7 @@ export async function getRecentTimetables() {
       responseType: "json",
     })
     .then((response) => response.data);
-  const mostUsed = Object.keys(data).filter((key) => data[key] > 50 && !key.includes("2023"));
+  const mostUsed = Object.keys(data).filter((key) => data[key] > 300 && key.includes("2023"));
   console.log("Most used count: ", mostUsed.length);
   mostUsed.unshift(
     "https://student2023.lpnu.ua/students_schedule?studygroup_abbrname=ПЗ-32&semestr=1&semestrduration=1"
